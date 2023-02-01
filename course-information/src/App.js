3b2f5cf0e466c5e0465d0ecad8874c053b81d94a
lokubet <prosto.sashaa@mail.ru>
2023-02-01 16:48:16 +0300

    2

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -6,14 +6,25 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return(
     <div>
-    <p>{props.part} {props.exercises}</p>
+      <p>{props.part} {props.exercises}</p>
     </div>
   )
 }
-const Total = (props) => {
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part = {props.part1} exercises = {props.exercises1} />
+      <Part part = {props.part2} exercises = {props.exercises2} />
+      <Part part = {props.part3} exercises = {props.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {  
   return (
     <div>
       <p>Number of exercises {props.a}</p>
@@ -36,9 +47,7 @@ const App = () => {
   return (
     <div>
       <Header h1 = 'Half Stack application development'/>
-        <Content part = {part1} exercises = {exercises1} />
-        <Content part = {part2} exercises = {exercises2} />
-        <Content part = {part3} exercises = {exercises3} />
+        <Content part1 = {part1} exercises1 = {exercises1} part2 = {part2} exercises2 = {exercises2} part3 = {part3} exercises3 = {exercises3}/>
         <Total a = {exercises1+exercises2+exercises3} />
     </div>
   )
